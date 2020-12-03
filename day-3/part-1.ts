3dc36ba85dc697f1bfbe76bea3a802b0dd978f07
rucin93 <rucin93@gmail.com>
2020-12-03 20:42:57 +0100

    some duplications fix

--- day-3/part-1.ts
+++ day-3/part-1.ts
@@ -4,20 +4,25 @@ const entries = getLinesFromFile(`${__dirname}/input.txt`)
 
 const lines = entries.length
 
-const checkGrid = (x, y) => {
+const checkGrid = (x: number, y :number) :number => {
     const grid = []
     for (let i = 0; i < x * lines; i++) {
-        grid[i] = ''.padStart(y * lines, entries[i])
+        grid[i] = ''.padStart( y * lines, entries[i])
     }
+
     let i = 0, j = 0;
-    const getNextPoint = () => grid[i].charAt(j, i+=x, j+=y)
+    const getNextPoint = () => grid[i].charAt(j, i += x, j += y)
 
     let counter = 0
-    for(let k = 0; k < lines; k++) {
-        if(getNextPoint() === '#') counter++;
+    for ( let k = 0; k < lines; k++ ) {
+        if ( getNextPoint() === '#' ) counter++
     }
 
     return counter
+}
+
+export {
+    checkGrid
 }
 
 console.log(checkGrid(1,3))
--- day-3/part-2.ts
+++ day-3/part-2.ts
@@ -1,24 +1,4 @@
-import { getLinesFromFile } from '../utils'
-
-const entries = getLinesFromFile(`${__dirname}/input.txt`)
-const lines = entries.length
-
-const checkGrid = (x, y) => {
-    const grid = []
-    for (let i = 0; i < x * lines; i++) {
-        grid[i] = ''.padStart( y * lines, entries[i])
-    }
-
-    let i = 0, j = 0;
-    const getNextPoint = () => grid[i].charAt(j, i += x, j += y)
-
-    let counter = 0
-    for ( let k = 0; k < lines; k++ ) {
-        if ( getNextPoint() === '#' ) counter++
-    }
-
-    return counter
-}
+import {checkGrid} from "./part-1";
 
 console.log([[1,1], [1,3], [1, 5], [1,7], [2,1]].map(set => {
     const [x, y] = set
